e8242e87b4d0eb6217e4386cc2afb15702798478
ckodes <ckodes@outlook.com>
2025-07-23 23:31:19 +1000

    feat(test): notion mock data

---
+++ src/lib/__mocks__/notionMockData.ts
@@ -0,0 +1,182 @@
+export const notionMockData = [
+  {
+    id: "123a7654-123b-1234-cd12345ef1ab",
+    properties: {
+      Status: {
+        type: "status",
+        status: {
+          name: "Published",
+        },
+      },
+      Summary: {
+        type: "rich_text",
+        rich_text: [
+          {
+            plain_text:
+              "Entry 1: Conditions where margin 0 auto requires to work",
+          },
+        ],
+      },
+      Tags: {
+        type: "multi_select",
+        multi_select: [
+          {
+            name: "CSS",
+          },
+          {
+            name: "Project",
+          },
+        ],
+      },
+      Date: {
+        type: "date",
+        date: {
+          start: "2025-07-17",
+        },
+      },
+      Title: {
+        id: "title",
+        type: "title",
+        title: [
+          {
+            type: "text",
+            text: {
+              content: "Entry 1: Centering with margin: 0 auto",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "Entry 1: Centering with margin: 0 auto",
+            href: null,
+          },
+        ],
+      },
+    },
+  },
+  {
+    id: "987a4567-123b-1234-cd12345ef1ab",
+    properties: {
+      Status: {
+        type: "status",
+        status: {
+          name: "Published",
+        },
+      },
+      Summary: {
+        type: "rich_text",
+        rich_text: [
+          {
+            plain_text:
+              "Entry 2: Conditions where margin 0 auto requires to work",
+          },
+        ],
+      },
+      Tags: {
+        type: "multi_select",
+        multi_select: [
+          {
+            name: "CSS",
+          },
+          {
+            name: "Project",
+          },
+        ],
+      },
+      Date: {
+        type: "date",
+        date: {
+          start: "2025-07-17",
+        },
+      },
+      Title: {
+        id: "title",
+        type: "title",
+        title: [
+          {
+            type: "text",
+            text: {
+              content: "Entry 2: Centering with margin: 0 auto",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "Entry 2: Centering with margin: 0 auto",
+            href: null,
+          },
+        ],
+      },
+    },
+  },
+  {
+    id: "123a4567-123b-1234-cd12345ef1ab",
+    properties: {
+      Status: {
+        type: "status",
+        status: {
+          name: "Published",
+        },
+      },
+      Summary: {
+        type: "rich_text",
+        rich_text: [
+          {
+            plain_text:
+              "Entry 3: Conditions where margin 0 auto requires to work",
+          },
+        ],
+      },
+      Tags: {
+        type: "multi_select",
+        multi_select: [
+          {
+            name: "CSS",
+          },
+          {
+            name: "Project",
+          },
+        ],
+      },
+      Date: {
+        type: "date",
+        date: {
+          start: "2025-07-17",
+        },
+      },
+      Title: {
+        id: "title",
+        type: "title",
+        title: [
+          {
+            type: "text",
+            text: {
+              content: "Entry 3: Centering with margin: 0 auto",
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: "Entry 3: Centering with margin: 0 auto",
+            href: null,
+          },
+        ],
+      },
+    },
+  },
+]
