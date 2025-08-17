03ab351050a560ed4105be58fbf62e5b669296d7
ckodes <ckodes@outlook.com>
2025-08-18 02:54:19 +0800

    feat: add basic BlogPage with ssr rendering

--- src/app/blogPage/[slug]/page.tsx
+++ src/app/blogPage/[slug]/page.tsx
@@ -1,9 +1,26 @@
-import React from "react"
+import { queryDatabase, queryPage } from "@lib/notion"
 
-export default function BlogPage() {
+export default async function BlogPage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const { slug } = params
+
+  const posts = await queryDatabase(process.env.NOTION_DATABASE_ID!)
+  const post = posts.find((p) => p.slug === slug)
+
+  if (!post) return <div>404: Post not found</div>
+
+  const pageData = await queryPage(post.pageId)
+
   return (
-    <div>
-      <h1>This is a BlogPage</h1>
-    </div>
+    <article>
+      <h1>{post.title}</h1>
+      <p>{post.date}</p>
+      <div>
+        <pre>{JSON.stringify(pageData, null, 2)}</pre>
+      </div>
+    </article>
   )
 }
