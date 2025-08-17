7cfe770d07908dc849f613143867602efe44c784
ckodes <ckodes@outlook.com>
2025-08-18 02:51:37 +0800

    feat: switch to ssr for data-fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,29 @@
-"use client"
-
 import Card, { CardProps } from "@components/Card/Card"
 import styles from "./page.module.css"
-import { useState, useEffect } from "react"
+import { queryDatabase } from "@lib/notion"
 
-export default function Home() {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [notionData, setNotionData] = useState<any>(null)
-
-  useEffect(() => {
-    async function fetchNotionData() {
-      try {
-        const res = await fetch("/api/notion", { method: "POST" })
-        const data = await res.json()
-        setNotionData(data)
-      } catch (error) {
-        console.error("Error fetching data:", error)
-      } finally {
-        console.log(fetchNotionData())
-      }
-    }
-
-    fetchNotionData()
-  }, [])
-  const toggleDarkMode = () => {
-    document.body.classList.toggle("dark-mode")
-  }
+export default async function Home() {
+  const notionData: CardProps[] = await queryDatabase(
+    process.env.NOTION_DATABASE_ID!
+  )
 
   return (
     <>
-      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
       <main className={styles.main}>
         <section className={styles.grid}>
-          {notionData &&
-            notionData.map((item: CardProps, i: number) => (
-              <Card
-                key={i}
-                title={item.title}
-                date={item.date}
-                summary={item.summary}
-                tags={item.tags}
-                pageId={item.pageId}
-                slug={item.slug}
-              ></Card>
-            ))}
+          {notionData.map((item, i) => (
+            <Card
+              key={i}
+              title={item.title}
+              date={item.date}
+              summary={item.summary}
+              tags={item.tags}
+              slug={item.slug}
+            />
+          ))}
         </section>
-
         <div>
-          {notionData && <pre>{JSON.stringify(notionData, null, 2)}</pre>}
+          <pre>{JSON.stringify(notionData, null, 2)}</pre>
         </div>
       </main>
     </>
