febbf261ed45afde2c6ac1ddf0cccbe3b2734eef
ckodes <ckodes@outlook.com>
2025-08-24 18:02:43 +0800

    feat(home): render Card and StickyNote based on status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,40 @@
-import Card, { CardProps } from "@components/Card/Card"
+import Card from "@components/Card/Card"
 import styles from "./page.module.css"
 import { queryDatabase } from "@lib/notion"
 import StickyNote from "@components/StickyNote"
 
 export default async function Home() {
-  const notionData: CardProps[] = await queryDatabase(
-    process.env.NOTION_DATABASE_ID!
-  )
+  const notionData = await queryDatabase(process.env.NOTION_DATABASE_ID!)
 
   return (
     <>
-      <main className={styles.main}>
+      <main>
         <section className={styles.cardGrid}>
-          {notionData.map((item, i) => (
-            <Card
-              key={i}
-              title={item.title}
-              date={item.date}
-              summary={item.summary}
-              tags={item.tags}
-              slug={item.slug}
-            />
-          ))}
+          {notionData
+            .filter((item) => item.status === "Published")
+            .map((item, i) => (
+              <Card
+                key={i}
+                title={item.title}
+                date={item.date}
+                summary={item.summary}
+                tags={item.tags}
+                slug={item.slug}
+              />
+            ))}
         </section>
         <section className={styles.stickyNoteGrid}>
-          {notionData.map((item, i) => (
-            <StickyNote key={i}>{item.summary}</StickyNote>
-          ))}
+          {notionData
+            .filter((item) => item.status === "In progress")
+            .map((item, i) => (
+              <StickyNote key={i}>{item.summary}</StickyNote>
+            ))}
         </section>
-        {/* <div>
-          <pre>{JSON.stringify(notionData, null, 2)}</pre>
-        </div> */}
+        {/* <section>
+          <div>
+            <pre>{JSON.stringify(notionData, null, 2)}</pre>
+          </div>
+        </section> */}
       </main>
     </>
   )
