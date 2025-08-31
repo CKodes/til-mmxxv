fab90147db8fffbf16e7bf07083c1be79b704f77
ckodes <ckodes@outlook.com>
2025-08-31 15:55:17 +0800

    fix: renderArticle based on type and wraps the spans

--- src/app/blogPage/[slug]/page.tsx
+++ src/app/blogPage/[slug]/page.tsx
@@ -14,13 +14,43 @@ export default async function BlogPage({
   if (!post) return <div>404: Post not found</div>
 
   const pageData = await queryPage(post.pageId)
-  const renderArticle = pageData.map((item) => (
-    <li className={styles.list} key={item.id}>
-      {item.spans.map((span, i) => (
-        <p key={`${item.id}-span-${i}`}>{span.text}</p>
-      ))}
-    </li>
-  ))
+
+  const renderArticle = pageData.map((item) => {
+    switch (item.type) {
+      case "heading_1":
+        return (
+          <h1 key={item.id} className={styles.headingStyles}>
+            {item.spans.map((span, i) => (
+              <span key={`${item.id}-span-${i}`}>{span.text}</span>
+            ))}
+          </h1>
+        )
+      case "heading_2":
+        return (
+          <h2 key={item.id} className={styles.headingStyles}>
+            {item.spans.map((span, i) => (
+              <span key={`${i}`}>{span.text}</span>
+            ))}
+          </h2>
+        )
+      case "heading_3":
+        return (
+          <h3 key={item.id} className={styles.headingStyles}>
+            {item.spans.map((span, i) => (
+              <span key={`${i}`}>{span.text}</span>
+            ))}
+          </h3>
+        )
+      case "paragraph":
+        return (
+          <p key={item.id} className={styles.paragraphStyles}>
+            {item.spans.map((span, i) => (
+              <span key={`${i}`}>{span.text}</span>
+            ))}
+          </p>
+        )
+    }
+  })
 
   return (
     <>
